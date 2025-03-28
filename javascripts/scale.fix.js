bc85404946ade9f1d58eeb189eb169a0424360cb
seowoochung <sc5130@columbia.edu>
2025-03-28 18:00:57 -0400

    Update scale.fix.js

--- javascripts/scale.fix.js
+++ javascripts/scale.fix.js
@@ -15,3 +15,11 @@ function gestureStart() {
     }
   }
 }
+function toggleAbstract(abstractId) {
+  var abstract = document.getElementById(abstractId);
+  if (abstract.style.display === "none") {
+    abstract.style.display = "block";
+  } else {
+    abstract.style.display = "none";
+  }
+}
